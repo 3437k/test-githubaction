3d89a9e522a233cc7cd7b366ce9263b6e9b78bc5
insort <insort@naver.com>
2023-10-09 21:39:18 +0900

    fix change image path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export" };
+
+const isProduction = process.env.NODE_ENV === "production";
+const repoName = isProduction ? "/test-githubaction" : "";
+
+const nextConfig = {
+  output: "export",
+  assetPrefix: repoName,
+};
 
 module.exports = nextConfig;
