edcffee2d86f0745177999b50005ede252f1bccb
insort <insort@naver.com>
2023-10-09 22:10:04 +0900

    fix remove assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+// assetPrefix
 // https://nextjs.org/docs/app/api-reference/next-config-js/assetPrefix
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-  assetPrefix: isProduction ? "/test-githubaction" : undefined,
+  assetPrefix: "/zzzzzzzz",
 };
 
 module.exports = nextConfig;
